84d7d893203f412a011cb672cd244a907429ebed
Hibi Miyazaki <hibi.miyazaki@gmail.com>
2015-12-20 22:28:30 +0900

    add domain, path and export-all option

--- README.md
+++ README.md
@@ -4,10 +4,14 @@ Exports your hackpads to markdown, html or plaintext.
 
 To run:
 
-    node index.js -c <your client ID> -s <your secret> --format <format to export in> <pad ID 1> <pad ID 2>...
+    node index.js -c <your client ID> -s <your secret> --format <format to export in> -d <your domain ID> -p <export directory> <pad ID 1> <pad ID 2>...
 
 You can get your client ID and secret on the [account settings](https://hackpad.com/ep/account/settings/).
 
 The format can be either `html`, `md`, or `txt`.
+
+The domain ID is `foo` if your domain is `foo.hackpad.com`.
+
+Exports all pad if pad ID ommited.
 
 Each input pad will be saved to `<pad ID>.<format>` in the cwd.
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var Hackpad = require("hackpad");
+var mkdirp = require("mkdirp")
 var fs = require("fs");
+var getDirName = require("path").dirname;
 
 var argv = require("optimist")
     .usage("Exports hackpad documents")
@@ -19,6 +21,16 @@ var argv = require("optimist")
         describe: "The format to export the hackpad in (either 'html', 'md' or 'txt' - defaults to 'html')"
     })
 
+    .options("d", {
+        alias: "domain",
+        describe: "The domain ID - defaults to blank"
+    })
+
+    .options("p", {
+        alias: "path",
+        describe: "The export path - defaults to domain or 'pads'"
+    })
+
     .demand("c", "s")
     .argv;
 
@@ -26,15 +38,44 @@ if(argv.format && !(argv.format in { html: true, md: true, txt: true })) {
     throw new Error("Format must be 'html', 'md' or 'txt'");
 }
 
-var client = new Hackpad(argv.client_id, argv.secret);
+var path = argv.path || argv.domain || "pads";
+var option = (argv.domain != null) ? { site: argv.domain } : {};
+
+var client = new Hackpad(argv.client_id, argv.secret, option);
 var format = argv.format || "html";
 
-argv._.forEach(function(padId) {
-    client.export(padId, null, format, function(error, response) {
-        if(error) {
-            console.error(error);
+var exportPads = function(ids) {
+  ids.forEach(function(padId) {
+      // Hackpad API's bug of text encoding
+      var fmt = (format == "md") ? "txt" : format;
+
+      client.export(padId, null, fmt, function(error, response) {
+          if(error) {
+              console.error(error);
+          } else {
+              var filePath = path + "/" + padId + "." + format;
+              console.log(filePath);
+
+              mkdirp(getDirName(filePath), function (err) {
+                  if (err) {
+                      console.error(err);
+                  } else {
+                      fs.writeFileSync(filePath, response);
+                  }
+              })
+          }
+      });
+  });
+};
+
+if (0 < argv._.length) {
+    exportPads(argv._);
+} else {
+    client.list(function(err, result) {
+        if (err) {
+            console.error(err);
         } else {
-            fs.writeFileSync(padId + "." + format, response);
+            exportPads(result);
         }
     });
-});
+}
